c32718540020ae714f30d3a9bb1e2c3c28c5ad2f
MQuy <sugiacupit@gmail.com>
2014-01-22 15:17:34 +0700

    fix bug in cached for

--- lib/munna/extensions/basic_method.rb
+++ lib/munna/extensions/basic_method.rb
@@ -5,8 +5,8 @@ module Munna
 				Proxy::Write.builder(munna_perform, self, Helper.normalize_params(opts), &block)
 			end
 
-			def cached_for(expires_in, opts={}, &block)
-				Proxy::Write.builder(munna_perform, self, Helper.normalize_params(opts.merge(:expires_in => expires_in)), &block)
+			def cached_for(expires_in, *opts, &block)
+				Proxy::Write.builder(munna_perform, self, Helper.normalize_params(opts).merge(:expires_in => expires_in), &block)
 			end
 
 			def delete_cached(*opts)
--- lib/munna/version.rb
+++ lib/munna/version.rb
@@ -1,3 +1,3 @@
 module Munna
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
